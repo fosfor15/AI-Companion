6320b37097183116ec38c6cdab881f1ab76d25f5
Dmitry Kostyuk <dima.kostyuk1508@gmail.com>
2023-10-07 12:24:51 +0300

    Build base routing

---
+++ app/(auth)/(routes)/login/page.tsx
@@ -0,0 +1,16 @@
+
+function Login() {
+    return (
+        <main className="flex flex-col items-center justify-between min-h-screen p-24 text-2xl">
+            <h1>
+                Login to AI Companion!
+            </h1>
+
+            <h5>
+                Unauthenticated
+            </h5>
+        </main>
+    );
+}
+
+export default Login;
---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,16 @@
+
+function Home() {
+    return (
+        <main className="flex flex-col items-center justify-between min-h-screen p-24 text-2xl">
+            <h1>
+                Hello from AI Companion!
+            </h1>
+
+            <h5>
+                Authenticated
+            </h5>
+        </main>
+    );
+}
+
+export default Home;
