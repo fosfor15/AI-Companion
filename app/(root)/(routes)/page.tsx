4b905991bc4d96aa527a3c474b61afc72eaf9693
Dmitry Kostyuk <dima.kostyuk1508@gmail.com>
2023-10-15 10:30:47 +0300

    Build companion card and search functionality

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -2,14 +2,42 @@ import prismaClient from '@/lib/prisma-client';
 
 import SearchInput from '@/components/search-input';
 import Categories from '@/components/categories';
+import Companions from '@/components/companions';
 
-async function RootPage() {
+interface RootPageProps {
+    searchParams: {
+        categoryId: string;
+        name: string;
+    };
+};
+
+async function RootPage({ searchParams }: RootPageProps) {
     const categories = await prismaClient.category.findMany();
+
+    const data = await prismaClient.companion.findMany({
+        where: {
+            categoryId: searchParams.categoryId,
+            name: {
+                search: searchParams.name
+            }
+        },
+        orderBy: {
+            createdAt: 'desc'
+        },
+        include: {
+            _count: {
+                select: {
+                    messages: true
+                }
+            }
+        }
+    });
 
     return (
         <div className="h-full p-4 space-y-2">
             <SearchInput />
             <Categories data={categories} />
+            <Companions data={data} />
         </div>
     );
 }
